ed0881d446e1b2ddc32ed54e7d20da97f81f9e84
tliuau <94107845+tliuau@users.noreply.github.com>
2022-03-11 15:02:17 +0800

    Update Note.java

--- Lab2/base/Note.java
+++ Lab2/base/Note.java
@@ -2,7 +2,7 @@ package base;
 
 import java.util.*;
 
-public class Note {
+public class Note implements Comparable<Note>{
     private Date date;
     private String title;
 
@@ -24,6 +24,21 @@ public class Note {
         if (!(obj instanceof Note)) return false;
         Note other = (Note) obj;
         return Objects.equals(title, other.title);
+    }
+
+    @Override
+    public int compareTo(Note o){
+        //Compares this note to note o based on date
+        //Return 1 of greater than o
+        //Return 0 = equal
+        //Return -1 if less than
+        String d1 = date.toString();
+        String d2 = o.date.toString();
+
+        if (d1.compareTo(d2) >= 1) return 1;
+        else if (d1.compareTo(d2) <= -1) return -1;
+        else return 0;
+
     }
     
 }
